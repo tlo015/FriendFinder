ac85c285329fba54e19863c62af0b533a3877222
tlo015 <tlo015@yahoo.com>
2018-05-27 01:16:50 -0700

    added app.post

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -10,35 +10,48 @@ module.exports = function(app) {
     app.get("/api/friends", function(req, res) {
     res.json(friends);
     });
+
+
+    //Post Route 
+    // Create New Friend Profile - takes in JSON input
+    app.post("/api/friends", function(req, res) {
+        //return user input and app user total 
+        var newFriend = req.body;
+        var newFriendAns = req.body.friendAns;
+        var totals = [];
+        var difference = 0;
+        var matchInd = 0;
+        var matchName = "";
+        var matchPhoto = "";
+        
+
+        //loop through all friends 
+        //loop through friend's ans 
+        //math.abs = always reutrns an absolute value 
+        //push the difference of each friend's score against new friend into a separate array 
+        //find the match by going through the totals array to find the smallest number 
+
+        for (var i=0; i<friendsArray.length; i++) {
+            for (var j=0; j<newFriendAns.length; j++) {
+                difference += (Math.abs(parseInt(friendsArray[i].friendAns[j]) - parseInt(newFriendAns[j])));
+            }
+            totals.push(difference)
+        }
+
+        console.log ("array of totals: ", totals)
+
+        for (var i=0; i<totals.length; i++) {
+            if(totals[i]<= total[matchInd]);
+            matchInd = i;
+                //display match with friend who has the smallest difference
+                matchName = friendsArray[matchInd].friendName;
+                matchPhoto = friendsArray[matchInd].friendPhoto;
+        }
+
+    
+        //push result into the survey page 
+        friends.push(req.body);
+        res.json({matchName:matchName, matchPhoto:matchPhoto});
+    });
+
 };
-
-//Post Route 
-// Create New Friend Profile - takes in JSON input
-// app.post("/api/friends", function(req, res) {
-//     //return user input and app user total (newFriend.friendAns):
-//     var userInput = req.body
-//     var userInputTotal = "";
-
-//     //loop through all friends 
-//     //loop through each friend's ans 
-//     for (var i=0; i<friendArray.length; i++) {
-//         for (var j=0; j<friendAns.length; j++) {
-//             console.log(friendAns.length[j]);
-//         }
-//     }
-    
-//     //display match with friend who has the smallest difference
-//     var matchName = "";
-//     var matchImg = "";
-    
-//     //add up total of each friendAns array
-//     console.log("stored totals: ", )
-    
-//     //compare totals of each friend with the current user 
-    
-
-    
-
-//     //push result into the survey page 
-//     friends.push(req.body);
-// });
